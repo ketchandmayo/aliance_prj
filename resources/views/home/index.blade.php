50779b6375335397bcef083438f8b4c16418e686
denis <toxinden@gmail.com>
2024-11-18 21:50:17 +0500

    Add project files

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="./css/base.css">
+        <link rel="stylesheet" href="./css/header.css">
+        <link rel="stylesheet" href="./css/menu.css">
+        <link rel="stylesheet" href="./css/footer.css">
+        <link rel="stylesheet" href="./css/popup.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script src="./scripts/base.js"></script>
+    </head>
+    <body>
+        <div id="header" class="header"></div>
+        <div id="menu" class="menu"></div>
+        <div id="add-comment" class="add-comment no-display"></div>
+
+        <div class="content">
+            <h2>Главная</h2>
+
+            <div class="paragraph">
+                <p>Данная страница является домашней страницей, и служит для перехода в остальные разделы.</p>
+                <p>Нажмите “Отзывы” для перехода к странице с отзывами.</p>
+                <p>Нажмите “Мой профиль” для просмотра своего профиля. (Отображается только в авторизованном варианте страницы)</p>
+                <p>Авторизуйтесь для просмотра своего профиля. (Отображается только в неавторизованном варианте страницы)</p>
+            </div>
+
+        </div>
+
+        <div id="footer" class="footer"></div>
+    </body>
+</html>
